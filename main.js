377b1db75e4a4058aa730e40acfdc1c355d814e7
khafizka <irinakhafi@gmail.com>
2023-01-12 03:39:52 -0500

    It is working

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 // add a DOMContentLoaded event listener to document
+let pause = false;
 document.addEventListener('DOMContentLoaded', () => {
     // store a reference to the timer
     const timer = document.getElementById('timer');
     // store a reference to the timer button
     const timerBtn = document.getElementById("action");
     // add a click event listener to timer button
+    //let pause = false;
     timerBtn.addEventListener('click', () => {
       if (timerBtn.innerHTML === "START") {
         timerBtn.innerHTML = "PAUSE";
-        countDown();
+        countDown(pause);
       } 
       else if(timerBtn.innerHTML === "PAUSE"){
-        //countDown()
-        timerBtn.innerHTML = "RESTART";
-      }else if(timerBtn.innerHTML === "RESTART"){
-        countDown();
+        pause = true
+        console.log("pause" + pause)
+        timerBtn.innerHTML = "CONTINUE";
+      }else if(timerBtn.innerHTML === "CONTINUE"){
+        pause = false
       }
-    
-    //}
     })
     
     })
@@ -26,7 +27,10 @@ document.addEventListener('DOMContentLoaded', () => {
     // extension: add event listener on 'x' button to close popup
     let distanceLog = [];
     function countDown(){
-
+        console.log("STARTED countDown")
+        // if(timerBtn.innerHTML === "PAUSE"){
+        //     clearInterval(x);
+        // }
           // initialize timer's text to 25 mins
       timer.innerHTML = '01:00'; 
       // storing the time when the timer button is clicked
@@ -36,9 +40,9 @@ document.addEventListener('DOMContentLoaded', () => {
       end.setTime(oldDateObj.getTime() + (1 * 60 * 1000));
       // Update the count down every 1 second
       function countingTime(){
-        if(timerBtn.innerHTML === "PAUSE"){
-            clearInterval(x);
-        }
+        console.log("entered countingTime " + pause)
+        if(!pause) {
+        console.log("entered the pause condition");
         // Get current time
         let now = new Date().getTime();
     
@@ -52,9 +56,6 @@ document.addEventListener('DOMContentLoaded', () => {
         distanceLog.push(distance);
     
         }
-    
-        
-    
         // Time calculations for minutes and seconds
         let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((distance % (1000 * 60)) / 1000);
@@ -77,7 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
           //later: add a pop up "Time for a break"/ sound if possible
           //openTheForm();
         }
-      }
+        }}
       let x = setInterval(countingTime, 1000);
     }
     
